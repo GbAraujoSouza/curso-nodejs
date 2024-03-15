a646dcd97101ef6a0441a56195e91e4738e117bc
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2024-03-15 09:20:22 -0300

    feat: clone boilerplate from repo and specify page name

--- 03-cli-tool/src/controllers/generateController.ts
+++ 03-cli-tool/src/controllers/generateController.ts
@@ -1,27 +1,30 @@
-import { EnumChoicesBoilerplate } from "src/enum/choices-boilerplate.enum";
-import { IAnswers } from "src/interface/answers.interface";
+import { EnumChoicesBoilerplate } from 'src/enum/choices-boilerplate.enum';
+import { IAnswers } from 'src/interface/answers.interface';
 import shellJs from 'shelljs';
-import { EnumGitRepos } from "src/enum/gitRepos.enum";
-import path from "path";
+import { EnumGitRepos } from 'src/enum/gitRepos.enum';
+import path from 'path';
+import fs from 'fs';
 
 class GenerateController {
   public gen(answers: IAnswers) {
     try {
       switch (answers.tech) {
         case EnumChoicesBoilerplate.NODE_TS:
-          shellJs.cd(path.resolve())
-          shellJs.exec(`git clone ${EnumGitRepos.NODE_TS}`)          
-          break;
+          shellJs.cd(path.resolve());
+          shellJs.exec(`git clone git@github.com:troquatte/boilerplate-typescript-nodejs.git ./${answers.projectName}`);
+          console.log('Boilerplate successfuly created');
+          
+          shellJs.exit(0);
         case EnumChoicesBoilerplate.SCSS:
-          shellJs.cd(path.resolve())
-          shellJs.exec(`git clone ${EnumGitRepos.SCSS}`)          
-          break;
+          shellJs.cd(path.resolve());
+          shellJs.exec(`git clone ${EnumGitRepos.SCSS}`);
+
+          shellJs.exit(0);
       }
     } catch (error) {
       console.log(error);
-      
     }
   }
 }
 
-export const GenFile = new GenerateController(); 
+export const GenFile = new GenerateController();
